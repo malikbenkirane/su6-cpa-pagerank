eeff3d44cd4981bf41964845611226c033835139
Malik BEN KIRANE <malik.ben_kirane@etu.upmc.fr>
2019-03-16 14:34:33 +0100

    Fix nodes counting

--- src/sizeof_graph.c
+++ src/sizeof_graph.c
@@ -12,39 +12,59 @@ unsigned
 sizeof_graph(ginfo_t *ginfo) {
 	unsigned u, v;
 	unsigned nl, nn;
-	unsigned map_alloc;
-	unsigned map_index;
-	unsigned *map;
-	unsigned *douts;
+	unsigned *map, *revmap;
+	unsigned mi, mi_max; // map index, map max index
+	unsigned ri, ri_max; // max node index, revmap max index
+	unsigned *out;
 	FILE *fd;
 
-	douts = (unsigned *)malloc(sizeof(*douts)*MIN_ALLOC);
-	map = (unsigned *)malloc(sizeof(*map)*MIN_ALLOC);
-	if (douts == NULL || map == NULL)
+	out = (unsigned *)malloc(sizeof(*out) * MIN_ALLOC);
+	map = (unsigned *)malloc(sizeof(*map) * MIN_ALLOC);
+	revmap = (unsigned *)malloc(sizeof(*revmap) * MIN_ALLOC);
+	if (out == NULL || map == NULL || revmap == NULL)
 		return ERR_ALLOC;
-	map_alloc = MIN_ALLOC;
-	map_index = 0;
+	for (unsigned i = 0; i < MIN_ALLOC; i++) {
+		out[i] = 0;
+		map[i] = 0;
+		revmap[i] = 0;
+	}
+	mi_max = MIN_ALLOC - 1;
+	ri_max = MIN_ALLOC - 1;
+	ri = 0;
+	mi = 0;
 	nl = 0;
 	nn = 0;
-	for (unsigned i = 0; i < map_alloc; i++)
-		douts[i] = 0;
 	if ((fd = fopen(FILENAME, "r")) == NULL)
 		return ERR_FILE_OPEN;
 	while (fscanf(fd, "%u %u", &u, &v) == 2) {
-		if (map_index > map_alloc - 1) {
-			map_alloc += MIN_ALLOC;
-			douts = realloc(douts, sizeof(*douts)*map_alloc);
-			map = realloc(map, sizeof(*map)*map_alloc);
+		if (mi > mi_max) {
+			mi_max += MIN_ALLOC;
+			out = realloc(out, sizeof(*out) * (mi_max+1));
+			map = realloc(map, sizeof(*map) * (mi_max+1));
 		}
-		map[map_index] = u;
-		if (douts[map_index++]++ > 0) nn++;
-		map[map_index++] = v;
+		if (u > ri) ri = u;
+		if (v > ri) ri = v;
+		if (ri > ri_max) {
+			ri_max = ri;
+			revmap = realloc(revmap, sizeof(*revmap) * (ri_max+1));
+		}
+		// count nodes
+		if (revmap[u] == 0) nn++;
+		if (revmap[v] == 0) nn++;
+		// register u
+		map[mi] = u;
+		out[mi]++;
+		revmap[u] = mi++;
+		// register v (out degree 0)
+		map[mi] = v;
+		revmap[v] = mi++;
+		// count link
 		nl++;
 	}
 	ginfo->nl = nl;
 	ginfo->nn = nn;
 	ginfo->map = map;
-	ginfo->out_degrees = douts;
+	ginfo->out_degrees = out;
 	return (0);
 }
 
