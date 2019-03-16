2f5e5c3ec224470c3a7d03f7f05c85ae4a22d879
Malik BEN KIRANE <malik.ben_kirane@etu.upmc.fr>
2019-03-16 18:25:57 +0100

    Add another version to compare memory use

--- src/sizeof_graph.c
+++ src/sizeof_graph.c
@@ -12,33 +12,55 @@ unsigned
 sizeof_graph(ginfo_t *ginfo) {
 	unsigned u, v;
 	unsigned nl, nn;
+#ifdef MAPNODES
 	unsigned *map, *revmap;
 	unsigned mi_max; // map index, map max index
 	unsigned ri, ri_max; // max node index, revmap max index
+#else
+	unsigned imax;
+	unsigned maxi;
+#endif
 	unsigned *out;
+	unsigned *in;
 	FILE *fd;
 
 	out = (unsigned *)malloc(sizeof(*out) * MIN_ALLOC);
+	in = (unsigned *)malloc(sizeof(*in) * MIN_ALLOC);
+#ifdef MAPNODES
 	map = (unsigned *)malloc(sizeof(*map) * MIN_ALLOC);
 	revmap = (unsigned *)malloc(sizeof(*revmap) * MIN_ALLOC);
-	if (out == NULL || map == NULL || revmap == NULL)
+	if (out == NULL || in == NULL || map == NULL || revmap == NULL)
 		return ERR_ALLOC;
+#else
+	if (out == NULL || in == NULL)
+		return ERR_ALLOC;
+#endif
 	for (unsigned i = 0; i < MIN_ALLOC; i++) {
 		out[i] = 0;
+		in[i] = 0;
+#ifdef MAPNODES
 		map[i] = 0;
 		revmap[i] = 0;
+#endif
 	}
+#ifdef MAPNODES
 	mi_max = MIN_ALLOC - 1;
 	ri_max = MIN_ALLOC - 1;
 	ri = 0;
+#else
+	maxi = MIN_ALLOC - 1;
+	imax = 0;
+#endif
 	nl = 0;
 	nn = 0;
 	if ((fd = fopen(FILENAME, "r")) == NULL)
 		return ERR_FILE_OPEN;
 	while (fscanf(fd, "%u %u", &u, &v) == 2) {
+#ifdef MAPNODES
 		if (nn > mi_max) {
 			mi_max += MIN_ALLOC;
 			out = realloc(out, sizeof(*out) * (mi_max+1));
+			in = realloc(in, sizeof(*in) * (mi_max+1));
 			map = realloc(map, sizeof(*map) * (mi_max+1));
 		}
 		if (u > ri) ri = u;
@@ -47,7 +69,16 @@ sizeof_graph(ginfo_t *ginfo) {
 			ri_max = ri;
 			revmap = realloc(revmap, sizeof(*revmap) * (ri_max+1));
 		}
-		// register u
+#else
+		if (u > imax) imax = u;
+		if (v > imax) imax = v;
+		if (imax > maxi) {
+			maxi = imax;
+			out = realloc(out, sizeof(*out) * (maxi+1));
+			in = realloc(in, sizeof(*in) * (maxi+1));
+		}
+#endif
+#ifdef MAPNODES
 		if (revmap[u] == 0) {
 			map[nn] = u;
 			revmap[u] = nn;
@@ -59,14 +90,23 @@ sizeof_graph(ginfo_t *ginfo) {
 			nn++;
 		}
 		out[revmap[u]]++;
-		// count link
+		in[revmap[v]]++;
+#else
+		if (in[u] == 0 && out[u] == 0) nn++;
+		if (in[v] == 0 && out[v] == 0) nn++;
+		out[u]++;
+		in[v]++;
+#endif
 		nl++;
 	}
 	ginfo->nl = nl;
 	ginfo->nn = nn;
+#ifdef MAPNODES
 	ginfo->map = map;
-	ginfo->exit = out;
-	free(revmap);
+	ginfo->revmap = revmap;
+#endif
+	ginfo->in = in;
+	ginfo->out = out;
 	return (0);
 }
 
--- src/sizeof_graph.h
+++ src/sizeof_graph.h
@@ -2,10 +2,14 @@
 #define __SIZEOF_GRAPH_H__
 
 typedef struct {
-	unsigned nl;     // number of links
-	unsigned nn;     // number of nodes
-	unsigned *exit;  // out degree array
-	unsigned *map;   // map to actual node ids array
+	unsigned nl;       // number of links
+	unsigned nn;       // number of nodes
+	unsigned *in;      // in degrees array
+	unsigned *out;     // out degrees array
+#ifdef MAPNODES
+	unsigned *map;     // map to actual node ids array
+	unsigned *revmap;  // map actual ids to virtual ids
+#endif
 } ginfo_t;
 
 
